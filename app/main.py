a527b27de0c116d538487d492848247218144471
gooish <iivari.mansikka@tuni.fi>
2021-03-27 20:26:52 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+ships = [
+    {
+    'id': 0,
+    'name': 'Ever Given',
+    'Stuck': 'Yes'
+    }
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Is The Ship Stuck API</h1>'''
+
+@app.route('/api/v1/resources/stuck/all', methods=['GET'])
+def api_all():
+    return jsonify(ships)
+
+if __name__ == "__main__":
+    app.run()
